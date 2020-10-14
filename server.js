b17dd9753fad22c6988f1707a5b2128d641931f8
AKen00 <ajken04@gmail.com>
2020-10-13 22:47:15 -0400

    test with new file name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animal');
+const { animals } = require('./data/animals');
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
